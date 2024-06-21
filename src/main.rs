c2118c143d5c0b8b0ca7adf91ec600b15246dc4f
qtfkwk <qtfkwk+docx-test@gmail.com>
2024-06-21 15:07:03 -0400

    003

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,21 @@ fn main() -> Result<()> {
 
     docx = docx.add_paragraph(Paragraph::new().add_run(Run::new().add_text("Hello")));
 
+    docx = docx.add_style(
+        Style::new("Heading 1", StyleType::Character)
+            .name("Heading 1")
+            .next("Body Text")
+            .size(18 * 2)
+            .bold(),
+    );
+    docx = docx.add_paragraph(
+        Paragraph::new()
+            .add_run(Run::new().add_text("Heading 1"))
+            .style("Heading 1"),
+    );
+
     docx.build()
-        .pack(std::fs::File::create("validator/002.docx")?)?;
+        .pack(std::fs::File::create("validator/003.docx")?)?;
 
     Ok(())
 }
