4918dc0313ec2ba244d214820db4b454fafd10c6
qtfkwk <qtfkwk+docx-test@gmail.com>
2024-06-21 14:59:30 -0400

    002

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use anyhow::Result;
 use docx_rs::*;
 
 fn main() -> Result<()> {
-    let docx = Docx::new();
+    let mut docx = Docx::new();
+
+    docx = docx.add_paragraph(Paragraph::new().add_run(Run::new().add_text("Hello")));
+
     docx.build()
-        .pack(std::fs::File::create("validator/001.docx")?)?;
+        .pack(std::fs::File::create("validator/002.docx")?)?;
+
     Ok(())
 }
